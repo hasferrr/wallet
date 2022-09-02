5b2becf8a59984f06562afee40d758a7dcacc57c
hasferrr <hasbi.f.hamd@gmail.com>
2022-09-02 13:53:10 +0700

    enable/show reports table

--- report.py
+++ report.py
@@ -14,14 +14,14 @@ def report():
     # Connect to database
     con, cur = connect_db()
 
-    # Query data [('Income', 10000.0), ('Gifts', 10000.0)] and store to list
+    # Query data account_name and its amount, and store to list ---> [('Income', 10000.0), ('Gifts', 10000.0)]
     res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ?", (session["user_id"], "Income"))
     income_name_amount = list(res.fetchall())
 
     res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ?", (session["user_id"], "Expense"))
     expense_name_amount = list(res.fetchall())
 
-    # Get all account_name and place to store total amount for each account [['Income', 0], ['Gifts', 0], ['Others', 0]]
+    # Get all account_name and place to store total amount for each account ---> [['Income', 0], ['Gifts', 0], ['Others', 0]]
     income_list_totalamount, expense_list_totalamount = account_name_list('', 0.0)
 
     # Store to LIST OF TOTAL AMOUNT for each account, and get its TOTAL
@@ -43,6 +43,17 @@ def report():
     for i in expense_list_totalamount:
         total_expense += i[1]
 
+    # Get percentage per category (Income/Expense)
+    j = 0
+    for i in income_list_totalamount:
+        income_list_totalamount[j].append(round(i[1] / total_income * 100, 2))
+        j += 1
+
+    j = 0
+    for i in expense_list_totalamount:
+        expense_list_totalamount[j].append(round(i[1] / total_expense * 100, 2))
+        j += 1
+
     # Format amount as IDR
     income_list_amount_f = list(income_list_totalamount)
     i = 0
@@ -61,7 +72,7 @@ def report():
     total_income_f = idr(total_income)
     total_expense_f = idr(total_expense)
 
-    # Get percentage
+    # Get cash flow bar percentage
     if total_income > total_expense:
         income_percent = 100
         try:
@@ -84,4 +95,4 @@ def report():
         diff = idr(0)
 
     con.close()
-    return render_template("report.html", total_income_f=total_income_f, total_expense_f=total_expense_f, diff=diff, income_percent=income_percent, expense_percent=expense_percent)
+    return render_template("report.html", total_income_f=total_income_f, total_expense_f=total_expense_f, diff=diff, income_percent=income_percent, expense_percent=expense_percent, income_list_amount_f=income_list_amount_f, expense_list_amount_f=expense_list_amount_f)
