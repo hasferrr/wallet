dee3b4c406fb9616edc78b58e57a1965d88001bd
hasferrr <hasbi.f.hamd@gmail.com>
2022-09-02 14:55:59 +0700

    enable filter button in report page

--- report.py
+++ report.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Blueprint, render_template, request, redirect, session
 
 from helpers import login_required, connect_db, account_name_list, idr
@@ -14,12 +16,68 @@ def report():
     # Connect to database
     con, cur = connect_db()
 
-    # Query data account_name and its amount, and store to list ---> [('Income', 10000.0), ('Gifts', 10000.0)]
-    res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ?", (session["user_id"], "Income"))
-    income_name_amount = list(res.fetchall())
+    # Query data account_name and its amount
+    # If THIS MONTH button was clicked OR
+    # User reached route via GET
+    if request.form.get('filter_btn') == 'This Month' or request.method == "GET":
+        year_now = str(datetime.now().year)
+        month_now = str(datetime.now().month)
+        date_filter = year_now + '%' + month_now + '%'
+        label = 'This Month'
 
-    res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ?", (session["user_id"], "Expense"))
-    expense_name_amount = list(res.fetchall())
+    # If ALL TIME button was clicked
+    elif request.form.get('filter_btn') == 'All Time':
+        date_filter = '%'
+        label = 'All Time'
+
+    # If THIS YEAR button was clicked
+    elif request.form.get('filter_btn') == 'This Year':
+        year_now = str(datetime.now().year)
+        date_filter = year_now + '%'
+        label = 'This Year'
+
+    # If LAST 30 DAYS button was clicked
+    elif request.form.get('filter_btn') == 'Last 30D':
+        label = "Last 30 Days"
+
+    # If user select the date to filter
+    elif request.form.get('filter_btn') == 'between':
+        start_date = request.form.get('start_date')
+        end_date = request.form.get('end_date')
+        label = 'Between ' + start_date + ' and ' + end_date
+
+    # Prevent error 500 for magical reason
+    else:
+        date_filter = '0'
+        label = None
+
+    # Query records, and store to list ---> [('Income', 10000.0), ('Gifts', 10000.0)]
+    if request.form.get('filter_btn') == 'between':
+        res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ? AND date BETWEEN ? AND ?",
+                            (session["user_id"], "Income", start_date, end_date))
+        income_name_amount = list(res.fetchall())
+
+        res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ? AND date BETWEEN ? AND ?",
+                            (session["user_id"], "Expense", start_date, end_date))
+        expense_name_amount = list(res.fetchall())
+
+    elif request.form.get('filter_btn') == 'Last 30D':
+        res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ? AND date BETWEEN date('now','localtime','-31 day') AND date('now','localtime')",
+                            (session["user_id"], "Income"))
+        income_name_amount = list(res.fetchall())
+
+        res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ? AND date BETWEEN date('now','localtime','-31 day') AND date('now','localtime')",
+                            (session["user_id"], "Expense"))
+        expense_name_amount = list(res.fetchall())
+
+    else:
+        res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ? AND date LIKE ?",
+                            (session["user_id"], "Income", date_filter))
+        income_name_amount = list(res.fetchall())
+
+        res = cur.execute("SELECT account_name, amount FROM records WHERE user_id = ? AND account_category = ? AND date LIKE ?",
+                            (session["user_id"], "Expense", date_filter))
+        expense_name_amount = list(res.fetchall())
 
     # Get all account_name and place to store total amount for each account ---> [['Income', 0], ['Gifts', 0], ['Others', 0]]
     income_list_totalamount, expense_list_totalamount = account_name_list('', 0.0)
@@ -46,12 +104,18 @@ def report():
     # Get percentage per category (Income/Expense)
     j = 0
     for i in income_list_totalamount:
-        income_list_totalamount[j].append(round(i[1] / total_income * 100, 2))
+        try:
+            income_list_totalamount[j].append(round(i[1] / total_income * 100, 2))
+        except ZeroDivisionError:
+            income_list_totalamount[j].append(0)
         j += 1
 
     j = 0
     for i in expense_list_totalamount:
-        expense_list_totalamount[j].append(round(i[1] / total_expense * 100, 2))
+        try:
+            expense_list_totalamount[j].append(round(i[1] / total_expense * 100, 2))
+        except ZeroDivisionError:
+            expense_list_totalamount[j].append(0)
         j += 1
 
     # Format amount as IDR
@@ -95,4 +159,4 @@ def report():
         diff = idr(0)
 
     con.close()
-    return render_template("report.html", total_income_f=total_income_f, total_expense_f=total_expense_f, diff=diff, income_percent=income_percent, expense_percent=expense_percent, income_list_amount_f=income_list_amount_f, expense_list_amount_f=expense_list_amount_f)
+    return render_template("report.html", total_income_f=total_income_f, total_expense_f=total_expense_f, diff=diff, income_percent=income_percent, expense_percent=expense_percent, income_list_amount_f=income_list_amount_f, expense_list_amount_f=expense_list_amount_f, label=label)
